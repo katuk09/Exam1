e25ea3fa9cda1d435791ec0f0116aa053c89ed50
Katuk <katuk@mail.ru>
2022-09-19 20:03:47 +0600

    A bit of dialogue elements added

--- Program.cs
+++ Program.cs
@@ -1,34 +1,66 @@
-﻿
-void CreateNewArray(string[] arrOld, int wordSize, int len)
+﻿string Choice()
 {
+    string answ = "";
+    Console.Write("Would you like to input your own array of strings (press y). If not, my array will be used. ");
+    answ = Console.ReadLine();
+    return answ;
+}
+
+string[] FillArray()
+{
+    Console.Write("Please input size of your string array: ");
+    int sizeArr = Convert.ToInt32(Console.ReadLine());
+    string[] arrTemp = new string[sizeArr];
+    for (int ind = 0; ind < sizeArr; ind++)
+    {
+        Console.Write("Please input your string array #: " + (ind + 1) + " ");
+        arrTemp[ind] = Console.ReadLine();
+    }
+    return arrTemp;
+}
+
+string[] FillArray2()
+{
+    Console.WriteLine("You chose my array of strings: ");
+    string[] arr2 = { "Sepulka", "is", "a", "prominent", "element", "of", "the", "civilization", "of", "Ardrites", "from", "the", "planet", "of", "Enteropia" };
+    return arr2;
+}
+
+void CreateNewArray(string[] arrOld)
+{
+    Console.Write("Please input max length of a string element in our new array: ");
+    int wordLen = Convert.ToInt32(Console.ReadLine());
+
+    int count = 0;
+    string[] arrNew;
+    for (int i = 0; i < arrOld.Length; i++)
+        if (arrOld[i].Length <= wordLen) count++;
+    Console.WriteLine();
+    Console.WriteLine($"New array contains {count} string elements (with {wordLen} and less symbols length):");
+
     int j = 0;
-    string[] arrNew = new string[len];
+    arrNew = new string[count];
     for (int i = 0; i < arrOld.Length; i++)
     {
-        if (arrOld[i].Length <= wordSize) arrNew[j] = arrOld[i];
-        Console.Write(arrNew[j] + " ");
+        if (arrOld[i].Length <= wordLen)
+        {
+            arrNew[j] = arrOld[i];
+            if (arrNew[j] == "") Console.Write("NULL, ");
+            else Console.Write(arrNew[j] + " ");
+            j++;
+        }
     }
 }
 
+string answer = Choice();
+string[] arr;
 
-Console.Write("Please input size of your string array: ");
-int sizeArr = Convert.ToInt32(Console.ReadLine());
-Console.Write("Please input max length of a string element in our new array: ");
-int wordLen = Convert.ToInt32(Console.ReadLine());
+if (answer == "y") arr = FillArray();
+else arr = FillArray2();
 
-string[] arr = new string[sizeArr];
-int count = 0;
-for (int ind = 0; ind < sizeArr; ind++)
-{
-    Console.Write("Please input your string array #: " + (ind + 1) + " ");
-    arr[ind] = Console.ReadLine();
-    if (arr[ind].Length < wordLen) count++;
-}
-
-for (int ind = 0; ind < sizeArr; ind++)
+for (int ind = 0; ind < arr.Length; ind++)
     Console.Write(arr[ind] + " ");
 
 Console.WriteLine();
-Console.WriteLine($"New array of max {wordLen} symbols contains {count} string elements");
-
-CreateNewArray(arr[], wordLen, count);
+Console.WriteLine();
+CreateNewArray(arr);
