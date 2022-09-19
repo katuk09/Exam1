eb08ad9c96c31ab5ec7ed478a533ce1e95aad68f
Katuk <katuk@mail.ru>
2022-09-19 16:07:42 +0600

    Start with filling array

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+/*arry[] CreateNewArray()
+{
+
+}
+*/
+Console.Write("Please input size of your string array: ");
+int sizeArr = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please input max length of a string element in our new array: ");
+int wordLen = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please input your string array: ");
+
+string[] arr = new string[sizeArr];
+
+for (int ind = 0; ind < sizeArr; ind++)
+{
+    string s = Console.ReadLine();
+}
+Console.WriteLine();
